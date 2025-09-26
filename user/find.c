efaeaff4020733d9ced6a42e11962a432a20986a
Adeena Arif <adeenaarif@Adeenas-MacBook-Air.local>
2025-09-26 22:49:22 +0500

    Implemented find with regex support for assignment1

--- user/find.c
+++ user/find.c
@@ -5,6 +5,46 @@
 
 char buf[512];
 
+int match(char *re, char *text);
+int matchhere(char*, char*);
+int matchstar(int, char*, char*);
+
+int
+match(char *re, char *text)
+{
+  if(re[0] == '^')
+    return matchhere(re+1, text);
+  do {  // must look at empty string
+    if(matchhere(re, text))
+      return 1;
+  } while(*text++ != '\0');
+  return 0;
+}
+
+int
+matchhere(char *re, char *text)
+{
+  if(re[0] == '\0')
+    return 1;
+  if(re[1] == '*')
+    return matchstar(re[0], re+2, text);
+  if(re[0] == '$' && re[1] == '\0')
+    return *text == '\0';
+  if(*text!='\0' && (re[0]=='.' || re[0]==*text))
+    return matchhere(re+1, text+1);
+  return 0;
+}
+
+int
+matchstar(int c, char *re, char *text)
+{
+  do {  // a * matches zero or more instances
+    if(matchhere(re, text))
+      return 1;
+  } while(*text!='\0' && (*text++==c || c=='.'));
+  return 0;
+}
+
 void find(char *path, char *name) {
   char *p;
   int fd;
@@ -28,7 +68,7 @@ void find(char *path, char *name) {
     while(p >= path && *p != '/')
       p--;
     p++;
-    if(strcmp(p, name) == 0){
+    if(match(name,p)){
       printf("%s\n", path);
     }
   }
@@ -53,7 +93,7 @@ void find(char *path, char *name) {
         }
 
         if(st.type == T_FILE){
-          if(strcmp(de.name, name) == 0){
+          if(match(de.name, name) == 0){
             printf("%s\n", buf);
           }
         } else if(st.type == T_DIR){
